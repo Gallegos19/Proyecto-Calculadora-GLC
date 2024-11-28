3fc5f9bb827ba603ddbfbe4dd213d3b9a6306dcb
Adrian <adriborraz04@gmail.com>
2024-11-28 09:58:25 -0600

    correccion de cambios en Parse_S

--- calculadora.py
+++ calculadora.py
@@ -132,8 +132,16 @@ class CalculatorParser:
 
     def parse_S(self):
         node = self.create_node('S')
-        child = self.parse_A()
-        self.add_edge(node, child)
+        # Manejar un signo negativo inicial
+        if self.get_current_token() == '-':
+            minus_node = self.create_node('-')
+            self.add_edge(node, minus_node)
+            self.consume_token()
+            child = self.parse_A()
+            self.add_edge(node, child)
+        else:
+            child = self.parse_A()
+            self.add_edge(node, child)
         return node
 
     def parse_A(self):
